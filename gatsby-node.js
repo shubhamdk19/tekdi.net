d9c655ab8a4d440f0fd5d821b8dc20d470223727
nilesh5995 <nilesh_s@techjoomla.com>
2020-07-08 19:42:14 +0530

    gatsby build is not working

--- gatsby-node.js
+++ gatsby-node.js
@@ -58,7 +58,7 @@ exports.createPages = ({ actions, graphql }) => {
           }
         }
       }
-      pages:allMarkdownRemark(limit: 1000, filter: {frontmatter: {templateKey: { in: ["blog-post", "case-study", "how", "positions", "platforms", "solutions", "digital-transformation", "analytics", "experience", "capabilities", "industries"]}}}) {
+      pages:allMarkdownRemark(limit: 1000, filter: {frontmatter: {templateKey: { in: ["blog-post", "case-study", "how", "positions", "platforms", "solutions", "digital-transformation", "analytics", "experience", "capabilities"]}}}) {
           edges {
             node {
               id
